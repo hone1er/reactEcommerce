4fae805e3e572ffe2fd1782af61ca21fc57219fc
hone1er <josephmvillavicencio@gmail.com>
2020-04-17 08:38:42 -1000

    added sweatshirts

--- src/components/StyledCard.js
+++ src/components/StyledCard.js
@@ -32,7 +32,6 @@ const StyledCard = styled.div`
     max-width: 100%;
     background: ${props => props.theme.secondary};
     color: ${props => props.theme.main};
-    padding: 10px;
     border-radius: 0 0 ${props => props.theme.borderRadius}
       ${props => props.theme.borderRadius};
     height: 130px;
@@ -41,8 +40,8 @@ const StyledCard = styled.div`
     h4,
     h2,
     p {
-      margin: 3px;
-      padding: 0 10px;
+      margin: 0px;
+      padding: 0 5px;
     }
     .sub {
       font-weight: bold;
@@ -74,9 +73,18 @@ const StyledCard = styled.div`
     }
 
     .cartCardInfo {
-      width: min-content;
+      width: 100%;
       padding: 5px;
       background: inherit;
+
+      h2 {
+        font-size: 1rem;
+      }
+    }
+
+    &.cart-item {
+      max-width: 100%;
+      width: 100%;
     }
   }
   @media (max-width: 646px) {
@@ -96,21 +104,24 @@ const StyledCard = styled.div`
   }
 
   @media (max-width: 480px) {
-    min-width: 48vw;
-    max-width: 48vw;
-    width: 48vw;
+    min-width: 50vw;
+    max-width: 50vw;
+    width: 50vw;
     height: 100%;
-    margin: 10px auto;
+    margin: 10px 0;
+    justify-content: center;
     .detail-img {
       width: 100%;
       max-width: 100%;
       min-width: 70vw;
     }
+    &.cart-item {
+      max-width: 100% !important;
+      width: 100% !important;
+    }
   }
 
   @media (max-width: 339px) {
-    width: 157px;
-    min-width: 157px;
     margin: 15px 1px;
   }
 `;
